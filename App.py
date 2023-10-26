a00b0252a86241e72a7056ff5014452e8f2fb7e1
Alessandro Coronado Siles <alecoronadosiles@hotmail.com>
2023-10-26 18:47:58 +0000

    hello

--- App.py
+++ App.py
@@ -1,51 +1,59 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
-
 def run():
+    # Set page config
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Análisis de Proyectos",
+        page_icon="📊",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    # Load the data
+    data = pd.read_excel("path_to_your_data/FECHAS.xlsx")
 
-    st.sidebar.success("Select a demo above.")
+    # App title and description
+    st.title("Análisis de Proyectos")
+    st.write("Análisis de la duración en meses entre diferentes etapas de los proyectos.")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    # User input for country selection
+    country = st.selectbox("Selecciona un país:", data['PAIS'].unique())
 
+    # User input for analysis type
+    analysis_type = st.selectbox("Selecciona el tipo de análisis:", ["CartaConsulta-Aprobación", "Aprobación-Vigencia", "Vigencia-Elegibilidad", "Elegibilidad-PrimeDesembolso"])
+
+    # Visualization
+    if analysis_type == "CartaConsulta-Aprobación":
+        column = 'Meses_CartaConsulta_Aprobacion'
+    elif analysis_type == "Aprobación-Vigencia":
+        column = 'Meses_Aprobacion_Vigencia'
+    elif analysis_type == "Vigencia-Elegibilidad":
+        column = 'Meses_Vigencia_Elegibilidad'
+    else:
+        column = 'Meses_Elegibilidad_PrimeDesembolso'
+
+    # Filter data by country
+    filtered_data = data[data['PAIS'] == country]
+
+    # Group by year of approval and calculate the mean of the selected column
+    grouped = filtered_data.groupby('AÑOAprobacion')[column].mean()
+
+    # Plotting
+    fig, ax = plt.subplots(figsize=(10, 6))
+    grouped.plot(kind='bar', ax=ax, color='lightblue')
+    ax.set_ylabel('Meses')
+    ax.set_xlabel('Año de Aprobación')
+    ax.set_title(f'{analysis_type} - {country}')
+    for bar in ax.patches:
+        yval = bar.get_height()
+        ax.text(bar.get_x() + bar.get_width()/2, yval + 0.1, round(yval, 0), ha='center', va='bottom')
+
+    st.pyplot(fig)
+
+    # Optional: Sidebar information or other components can be added here
 
 if __name__ == "__main__":
     run()
