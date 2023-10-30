ba56284ed4540f6df7cf117ad1af8bbc14db31b6
Alessandro Coronado Siles <alecoronadosiles@hotmail.com>
2023-10-30 19:37:16 +0000

    plot

--- Hello.py
+++ Hello.py
@@ -3,8 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
-
 def run():
     # Set page config
     st.set_page_config(
@@ -12,48 +10,52 @@ def run():
         page_icon="📊",
     )
 
-    # Load the data
-    data = pd.read_excel("/FECHAS.xlsx")
+    # Upload the Excel file
+    uploaded_file = st.file_uploader("Carga tu archivo Excel", type=["xlsx"])
 
-    # App title and description
-    st.title("Análisis de Proyectos")
-    st.write("Análisis de la duración en meses entre diferentes etapas de los proyectos.")
+    if uploaded_file is not None:
+        data = pd.read_excel(uploaded_file)
 
-    # User input for country selection
-    country = st.selectbox("Selecciona un país:", data['PAIS'].unique())
+        # App title and description
+        st.title("Análisis de Proyectos")
+        st.write("Análisis de la duración en meses entre diferentes etapas de los proyectos.")
 
-    # User input for analysis type
-    analysis_type = st.selectbox("Selecciona el tipo de análisis:", ["CartaConsulta-Aprobación", "Aprobación-Vigencia", "Vigencia-Elegibilidad", "Elegibilidad-PrimeDesembolso"])
+        # User input for country selection
+        country = st.selectbox("Selecciona un país:", data['PAIS'].unique())
 
-    # Visualization
-    if analysis_type == "CartaConsulta-Aprobación":
-        column = 'Meses_CartaConsulta_Aprobacion'
-    elif analysis_type == "Aprobación-Vigencia":
-        column = 'Meses_Aprobacion_Vigencia'
-    elif analysis_type == "Vigencia-Elegibilidad":
-        column = 'Meses_Vigencia_Elegibilidad'
-    else:
-        column = 'Meses_Elegibilidad_PrimeDesembolso'
+        # User input for analysis type
+        analysis_type = st.selectbox("Selecciona el tipo de análisis:", ["CartaConsulta-Aprobación", "Aprobación-Vigencia", "Vigencia-Elegibilidad", "Elegibilidad-PrimeDesembolso"])
 
-    # Filter data by country
-    filtered_data = data[data['PAIS'] == country]
+        # Visualization
+        if analysis_type == "CartaConsulta-Aprobación":
+            column = 'Meses_CartaConsulta_Aprobacion'
+        elif analysis_type == "Aprobación-Vigencia":
+            column = 'Meses_Aprobacion_Vigencia'
+        elif analysis_type == "Vigencia-Elegibilidad":
+            column = 'Meses_Vigencia_Elegibilidad'
+        else:
+            column = 'Meses_Elegibilidad_PrimeDesembolso'
 
-    # Group by year of approval and calculate the mean of the selected column
-    grouped = filtered_data.groupby('AÑOAprobacion')[column].mean()
+        # Filter data by country
+        filtered_data = data[data['PAIS'] == country]
 
-    # Plotting
-    fig, ax = plt.subplots(figsize=(10, 6))
-    grouped.plot(kind='bar', ax=ax, color='lightblue')
-    ax.set_ylabel('Meses')
-    ax.set_xlabel('Año de Aprobación')
-    ax.set_title(f'{analysis_type} - {country}')
-    for bar in ax.patches:
-        yval = bar.get_height()
-        ax.text(bar.get_x() + bar.get_width()/2, yval + 0.1, round(yval, 0), ha='center', va='bottom')
+        # Group by year of approval and calculate the mean of the selected column
+        grouped = filtered_data.groupby('AÑOAprobacion')[column].mean()
 
-    st.pyplot(fig)
+        # Plotting
+        fig, ax = plt.subplots(figsize=(10, 6))
+        grouped.plot(kind='bar', ax=ax, color='lightblue')
+        ax.set_ylabel('Meses')
+        ax.set_xlabel('Año de Aprobación')
+        ax.set_title(f'{analysis_type} - {country}')
+        for bar in ax.patches:
+            yval = bar.get_height()
+            ax.text(bar.get_x() + bar.get_width()/2, yval + 0.1, round(yval, 0), ha='center', va='bottom')
 
-    # Optional: Sidebar information or other components can be added here
+        st.pyplot(fig)
+
+        # Optional: Sidebar information or other components can be added here
 
 if __name__ == "__main__":
     run()
+
