a1624c72a6a59d9d2b6cd53c876474ed9f6c8632
Alessandro Coronado Siles <alecoronadosiles@hotmail.com>
2023-11-09 16:45:39 +0000

    Crear estaciones

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -1,77 +1,101 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from urllib.error import URLError
-
-import altair as alt
-import pandas as pd
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+import pandas as pd
+import io
 
+# Función para calcular la diferencia en meses entre dos fechas
+def calculate_kpi(end_date, start_date):
+    if pd.isnull(start_date) or pd.isnull(end_date):
+        return None  # Si alguna de las fechas está vacía, el KPI no se calcula
+    return round(((end_date - start_date).days / 30), 2)
 
-def data_frame_demo():
-    @st.cache_data
-    def get_UN_data():
-        AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-        df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-        return df.set_index("Region")
+# Función para calcular la productividad basada en el KPI
+def calculate_productivity(kpi):
+    if kpi is None:
+        return "Datos insuficientes"
+    if kpi < 6:
+        return "Eficiente"
+    elif kpi < 8:
+        return "Aceptable"
+    elif kpi < 12:
+        return "Con Demora"
+    else:
+        return "Alta Demora"
 
-    try:
-        df = get_UN_data()
-        countries = st.multiselect(
-            "Choose countries", list(df.index), ["China", "United States of America"]
+# Función para obtener el año de la fecha correspondiente a la operación
+def get_year_for_operation(date):
+    return date.year if pd.notnull(date) else None
+
+# Función para obtener solo la primera palabra del nombre de la estación
+def get_first_word(station):
+    return station.split()[0] if station else None
+
+# Función principal de la app de Streamlit
+def run():
+    st.set_page_config(page_title="Análisis de Eficiencia Operativa", page_icon="📊")
+
+    uploaded_file = st.file_uploader("Carga tu archivo Excel", type=["xlsx"])
+
+    if uploaded_file is not None:
+        data = pd.read_excel(uploaded_file)
+
+        # Convertir las columnas de fecha a datetime y extraer el año
+        date_columns = ['FechaCartaConsulta', 'FechaAprobacion', 'FechaVigencia', 'FechaElegibilidad', 'FechaPrimeDesembolso']
+        for col in date_columns:
+            data[col] = pd.to_datetime(data[col], errors='coerce')
+
+        # Mapeo de estaciones a sus respectivas columnas de fecha
+        operations = {
+            'Elegibilidad - Vigencia': ('FechaVigencia', 'FechaElegibilidad'),
+            'PrimerDesembolso - Elegibilidad': ('FechaElegibilidad', 'FechaPrimeDesembolso'),
+            'Vigencia - Aprobacion': ('FechaAprobacion', 'FechaVigencia'),
+            'Aprobacion - Carta Consulta': ('FechaCartaConsulta', 'FechaAprobacion')
+        }
+
+        # Lista para almacenar los resultados
+        results = []
+
+        # Procesar las filas del DataFrame
+        for index, row in data.iterrows():
+            for operation, (start_col, end_col) in operations.items():
+                kpi = calculate_kpi(row[end_col], row[start_col])
+                productividad = calculate_productivity(kpi)
+                year = get_year_for_operation(row[end_col])
+                results.append({
+                    'ESTACIONES': get_first_word(operation),
+                    'ANO': year,
+                    'PAIS': row['PAIS'],
+                    'CODIGO': row['NO. OPERACION'],
+                    'APODO': row['APODO'],
+                    'Indicador_Principal': row[end_col].strftime('%d/%m/%Y') if pd.notnull(row[end_col]) else None,
+                    'Indicador_Secundario': row[start_col].strftime('%d/%m/%Y') if pd.notnull(row[start_col]) else None,
+                    'TIPO_DE_KPI': operation,
+                    'KPI': kpi,
+                    'Productividad': productividad
+                })
+
+        # Convertir la lista de resultados en un DataFrame
+        results_df = pd.DataFrame(results)
+
+        # Mostrar el DataFrame en la aplicación
+        st.write("Datos Procesados:")
+        st.dataframe(results_df)
+
+        # Convertir el DataFrame a un archivo de Excel para la descarga
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine='openpyxl') as writer:
+            results_df.to_excel(writer, index=False)
+        st.download_button(
+            label="Descargar como Excel",
+            data=output.getvalue(),
+            file_name='resultados_kpi_productividad.xlsx',
+            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         )
-        if not countries:
-            st.error("Please select at least one country.")
-        else:
-            data = df.loc[countries]
-            data /= 1000000.0
-            st.write("### Gross Agricultural Production ($B)", data.sort_index())
 
-            data = data.T.reset_index()
-            data = pd.melt(data, id_vars=["index"]).rename(
-                columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-            )
-            chart = (
-                alt.Chart(data)
-                .mark_area(opacity=0.3)
-                .encode(
-                    x="year:T",
-                    y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                    color="Region:N",
-                )
-            )
-            st.altair_chart(chart, use_container_width=True)
-    except URLError as e:
-        st.error(
-            """
-            **This demo requires internet access.**
-            Connection error: %s
-        """
-            % e.reason
-        )
+if __name__ == "__main__":
+    run()
 
 
-st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
-st.markdown("# DataFrame Demo")
-st.sidebar.header("DataFrame Demo")
-st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
-)
 
-data_frame_demo()
 
-show_code(data_frame_demo)
+
+
